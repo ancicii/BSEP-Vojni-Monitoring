93c31a15c1dfaa9c49bee7fd72413b1fc5c78ba9
ancicii <ana.anchy.pjevic@gmail.com>
2020-05-15 18:27:52 +0200

    https

---
+++ SIEM-center/src/test/java/bsep/SIEMcenter/SiemCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package bsep.SIEMcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiemCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
